ea8969682db2639daef888cb3e66bd78c0ed9400
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-15 16:40:15 -0300

    ✨ feat: add message to database when completing the response

--- themis_backend/application/routes/message.py
+++ themis_backend/application/routes/message.py
@@ -1,10 +1,44 @@
+from starlette.background import BackgroundTask
 from starlette.requests import Request
 from starlette.responses import Response, StreamingResponse
 
+from themis_backend.application.adapters import request_adapter
+from themis_backend.application.factories import authenticate_composer
+from themis_backend.domain.services import BufferedGenerator
+from themis_backend.external.repositories.postgre_message_repository import (
+    PostgreMessageRepository,
+)
+from themis_backend.presentation.controllers import QuestionController
+from themis_backend.presentation.dtos import UserViewDTO
+
+
+async def store_message(
+    user_view: UserViewDTO, question: str, generator: BufferedGenerator
+):
+    repository = PostgreMessageRepository()
+    await repository.create(
+        user_id=user_view.user_id,
+        question=question,
+        answer=generator.get_text(),
+    )
+
 
 async def question_route(request: Request) -> Response:
-    question = (await request.json()).get('question')
+    response = await request_adapter(
+        request, QuestionController(), middlewares=[authenticate_composer()]
+    )
+
+    question = response.body['data']['question']
+    generator = request.app.model.generate(question)
+    task = BackgroundTask(
+        store_message,
+        generator=generator,
+        question=question,
+        user_view=response.authorization,
+    )
 
     return StreamingResponse(
-        request.app.model.generate(question), media_type='text/plain'
+        generator,
+        media_type='text/plain',
+        background=task,
     )
