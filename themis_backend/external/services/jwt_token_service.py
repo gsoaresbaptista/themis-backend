645d1fb622be626fc878f0f517f45bb65c3d5f56
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-15 16:34:57 -0300

    ♻️ refactor: improve jwt token service

--- themis_backend/external/services/jwt_token_service.py
+++ themis_backend/external/services/jwt_token_service.py
@@ -1,21 +1,39 @@
 from datetime import datetime, timedelta
+from typing import Optional
 
-from jose import jwt
+from jose import JWTError, jwt
 
 from themis_backend.config import TokenSettings
 from themis_backend.domain.services import AccessTokenService
-from themis_backend.presentation.dtos import UserViewDTO
+from themis_backend.presentation.dtos import TokenDTO, UserViewDTO
 
 
 class JTWAccessTokenService(AccessTokenService):
+    def __init__(self) -> None:
+        self.__expire_minutes = TokenSettings.ACCESS_TOKEN_EXPIRE_MINUTES
+        self.__algorithm = TokenSettings.ALGORITHM
+        self.__secret_key = TokenSettings.SECRET_KEY
+
     def create(self, user: UserViewDTO) -> str:
-        expire_minutes = TokenSettings.ACCESS_TOKEN_EXPIRE_MINUTES
-        algorithm = TokenSettings.ALGORITHM
-        secret_key = TokenSettings.SECRET_KEY
 
         data = user._asdict()
-        expire = datetime.utcnow() + timedelta(minutes=expire_minutes)
+        expire = datetime.utcnow() + timedelta(minutes=self.__expire_minutes)
         data.update({'exp': expire})
-        encoded_jwt = jwt.encode(data, secret_key, algorithm=algorithm)
+        encoded_jwt = jwt.encode(
+            data, self.__secret_key, algorithm=self.__algorithm
+        )
 
         return encoded_jwt
+
+    def decode(self, token: str) -> Optional[TokenDTO]:
+        try:
+            payload = jwt.decode(
+                token, key=self.__secret_key, algorithms=self.__algorithm
+            )
+            return TokenDTO(
+                user_id=payload['id'],
+                name=payload['name'],
+                email=payload['email'],
+            )
+        except JWTError:
+            return None
