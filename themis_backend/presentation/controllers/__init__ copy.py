2ad6251111eba4bedb9ccb87dd78e3212f0888d9
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-15 16:31:06 -0300

    🔥 remove: delete incorrect file

--- themis_backend/presentation/controllers/__init__ copy.py
+++
@@ -1,9 +0,0 @@
-from .controller import Controller
-from .create_user import CreateUserController
-from .sign_in import SignInController
-
-__all__ = [
-    'Controller',
-    'CreateUserController',
-    'SignInController',
-]
