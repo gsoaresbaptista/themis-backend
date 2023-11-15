171cdccb8e1b3e5bd2ea9f24e288193ee9cf6ae2
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-15 12:10:42 -0300

    🐛 fix: add model service in init file

--- themis_backend/domain/services/__init__.py
+++ themis_backend/domain/services/__init__.py
@@ -1,7 +1,9 @@
 from .access_token_service import AccessTokenService
 from .hash_service import HashService
+from .model_service import ModelService
 
 __all__ = [
     'HashService',
     'AccessTokenService',
+    'ModelService',
 ]
