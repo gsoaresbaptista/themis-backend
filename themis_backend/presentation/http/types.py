058f07c2e71a905e29a28d49498b0698ec64d1a0
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-15 16:30:24 -0300

    ♻️ refactor: change HTTP types from NamedTuple to dataclass

--- themis_backend/presentation/http/types.py
+++ themis_backend/presentation/http/types.py
@@ -1,10 +1,18 @@
-from typing import Any, NamedTuple, Optional
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from themis_backend.presentation.dtos import TokenDTO
 
 
-class HttpRequest(NamedTuple):
+@dataclass
+class HttpRequest:
     body: Optional[Any]
+    header: dict['str', Any]
+    authorization: Optional[TokenDTO] = None
 
 
-class HttpResponse(NamedTuple):
+@dataclass
+class HttpResponse:
     status_code: int
     body: Any
+    authorization: Optional[TokenDTO] = None
