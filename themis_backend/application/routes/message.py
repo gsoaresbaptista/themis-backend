85837ed1b5523ba96195cebc2fa7303cf192dc32
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-15 12:37:20 -0300

    🐛 fix: update model call pattern

--- themis_backend/application/routes/message.py
+++ themis_backend/application/routes/message.py
@@ -6,5 +6,5 @@ async def question_route(request: Request) -> Response:
     question = (await request.json()).get('question')
 
     return StreamingResponse(
-        request.app.model(question, stream=True), media_type='text/plain'
+        request.app.model.generate(question), media_type='text/plain'
     )
