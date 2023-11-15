dc6baa6bdb8ef90b370846c97665ee907cd63097
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-15 11:03:36 -0300

    🐛 fix: add route empty body check

--- themis_backend/application/adapters/request_adapter.py
+++ themis_backend/application/adapters/request_adapter.py
@@ -1,13 +1,22 @@
+from json import JSONDecodeError
+
 from starlette.requests import Request
 
 from themis_backend.presentation.controllers import Controller
 from themis_backend.presentation.http import HttpRequest, HttpResponse
+from themis_backend.presentation.http.errors import HTTPBadRequest
 
 
 async def request_adapter(
     request: Request, controller: Controller
 ) -> HttpResponse:
-    body = await request.json()
+    body = {}
+
+    if request.method != 'GET':
+        try:
+            body = await request.json()
+        except JSONDecodeError:
+            raise HTTPBadRequest()
 
     http_request = HttpRequest(body=body)
     http_response = await controller.handle(http_request)
--- themis_backend/application/setup/setup_routes.py
+++ themis_backend/application/setup/setup_routes.py
@@ -9,6 +9,6 @@ from themis_backend.application.routes import (
 
 # TODO: add openapi and an interface
 def setup_routes(app: Starlette) -> None:
-    app.add_route('/users', create_user_route, methods=['POST'])
+    app.add_route('/users/sign-up', create_user_route, methods=['POST'])
     app.add_route('/users/sign-in', sign_in_route, methods=['POST'])
     app.add_route('/messages/question', question_route, methods=['POST'])
